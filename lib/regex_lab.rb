740f301868eb4e3d4df46ca9175a2bc521b368f9
Agnes <agsawicka@gmail.com>
2022-08-11 14:53:20 -0400

    commit tree e6ee99057c89578fa31387317048cfddf1bcb609

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
 def starts_with_a_vowel?(word)
-
+    word.each do |char|
+        if char[0].match(/[aoeiu]/)
+            true
+        else 
+            false
+        end
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
